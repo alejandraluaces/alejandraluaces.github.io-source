8693a3f1a56fc60afb902bce568347ad73b31333
Alejandra Luaces <aluacesr@gmail.com>
2015-06-01 18:31:24 -0500

    Adds blog functionality and basic layout

--- config.rb
+++ config.rb
@@ -4,9 +4,9 @@
 
 # Time.zone = "UTC"
 
-# activate :blog do |blog|
+activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blog"
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
@@ -20,16 +20,20 @@
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  # blog.tag_template = "tag.html"
-  # blog.calendar_template = "calendar.html"
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
 
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
-# end
+end
+
+page "/feed.xml", layout: false
 
 # page "/feed.xml", layout: false
+page "/blog", :layout => :blog_post_layout
+page "/blog/*", :layout => :blog_post_layout
 
 ###
 # Compass
