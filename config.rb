e98d05b12e195d8e51b4a6ef1afbda833d30013c
Alejandra Luaces <aluacesr@gmail.com>
2015-06-02 10:23:41 -0500

    Cleans up config

--- config.rb
+++ config.rb
@@ -1,40 +1,24 @@
 ###
-# Blog settings
+# Blog
 ###
 
-# Time.zone = "UTC"
-
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
+
   blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
 
-#page "*", :layout => :article_layout
 page "/blog", :layout => :blog_post_layout
 page "/blog/*", :layout => :blog_post_layout
 page "/portfolio", :layout => :portfolio_layout
+
+###
+# Output
+###
 
 activate :autoprefixer
 activate :imageoptim
@@ -53,7 +37,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-# Build-specific configuration
+###
+# Build
+###
 
 configure :build do
 
